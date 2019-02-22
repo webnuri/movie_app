fcc2a3596b97979c42b690ae106bc48ee06191c0
webnuri <usaki72@naver.com>
2019-02-22 19:38:10 +0900

    Updates

---
+++ precache-manifest.9752e23faa610d321a646a245e8d657f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ec407ecfd48548ccf21",
+    "url": "/movie_app/static/css/main.ecdc5b0f.chunk.css"
+  },
+  {
+    "revision": "2ec407ecfd48548ccf21",
+    "url": "/movie_app/static/js/main.2ec407ec.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "e2a4fca7c1285f971d4a",
+    "url": "/movie_app/static/js/2.e2a4fca7.chunk.js"
+  },
+  {
+    "revision": "90ad2f3f934c326fefb94b58cebd66d3",
+    "url": "/movie_app/index.html"
+  }
+];
